bb84df930119aec00387fe980f1501fed96d89f3
Lushi Li <lushili06@gmail.com>
2013-03-26 12:47:49 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsMake24::Application.config.secret_token = '3cce8d2e705150796bb5be659e6250997cc02db474837255a434b6feb333722e4901ef19f4a2c845e37f84d69424a47f81c38af8a05fb11c411c0cb2d2f4e904'
