d1d3026f23a42292188e3163b28a893bedbab78e
Lushi Li <lushili06@gmail.com>
2013-03-28 14:43:52 -0400

    remove db migration for player_answer

--- db/migrate/20130328154828_add_player_answer_to_games.rb
+++
@@ -1,5 +0,0 @@
-class AddPlayerAnswerToGames < ActiveRecord::Migration
-  def change
-    add_column :games, :player_answer, :string
-  end
-end
--- db/migrate/20130328171802_remove_player_answer_from_games.rb
+++
@@ -1,9 +0,0 @@
-class RemovePlayerAnswerFromGames < ActiveRecord::Migration
-  def up
-  	add_column :games, :player_answer, :string
-  end
-
-  def down
-		remove_column :games, :player_answer
-  end
-end
