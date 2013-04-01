f0ba683718881309dad30c0e02f49d0f81ec5311
Lushi Li <lushili06@gmail.com>
2013-04-01 19:21:22 -0400

    bugfix in FindSolution; now all possible combo explored

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,6 @@
 class Game < ActiveRecord::Base
   attr_accessible :deck, :score, :session_id, :hand
-  #TODO: Use Marhsal to save game state as one string
+
   validates :score, presence: true
   validates :session_id, presence: true, uniqueness: true
 
@@ -68,7 +68,7 @@ class Game < ActiveRecord::Base
 	end
 
 	def solution
-		FindSolution.new(cards).print_solution
+		FindSolution.new(cards).solution
 	end
 
 	def over?
@@ -81,52 +81,36 @@ end
 
 class FindSolution
 	attr_reader :numbers
+
+	EXPRESSIONS = [
+		'((%d %s %d) %s %d) %s %d',
+		'(%d %s (%d %s %d)) %s %d',
+		'(%d %s %d) %s (%d %s %d)',
+		'%d %s ((%d %s %d) %s %d)',
+		'%d %s (%d %s (%d %s %d))'
+	]
+
+	OPERATORS = %w(+ - * /)
+
 	def initialize(num_array)
-		@numbers = num_array.map(&:to_f).sort.reverse
+		@numbers = num_array
 	end
 
-	def operators
-		%w(+ - * /)
-	end
-
-	def apply_ops_seq(number, op)
-		number[0].send(op[0], number[1]).
-			send(op[1], number[2]).
-			send(op[2], number[3])
-	end
-
-	def apply_ops_nonseq(number, op)
-		number[0].send(op[0], number[1]).
-			send(op[1], number[2].send(op[2], number[3]))
-	end
-
-	def good_permutation(nums, ops)
-		(0 if 24 == apply_ops_seq(nums, ops)) || (1 if 24 == apply_ops_nonseq(nums,ops))
+	def good_permutation?(test)
+		eval(test) == 24
 	rescue ZeroDivisionError
 		nil
 	end
 
 	def solution
-		numbers.permutation 4 do |nums|
-			operators.repeated_permutation 3 do |ops|
-				if good_permutation(nums, ops)
-					return [nums.map(&:to_i), ops, good_permutation(nums, ops)]
+		numbers.permutation do |a,b,c,d|
+			OPERATORS.repeated_permutation(3) do |ops1,ops2,ops3|
+				EXPRESSIONS.each do |exp|
+					test = exp.gsub('%d', 'Rational(%d,1)') % [a, ops1, b, ops2, c, ops3, d]
+					return exp % [a, ops1, b, ops2, c, ops3, d] if good_permutation?(test)
 				end
 			end
 		end
 		nil
-	end
-
-	def print_solution
-		s = solution
-		add_paren(s) if s
-	end
-
-	def add_paren(s)
-		if s[2] == 0
-			"((#{s[0][0]} #{s[1][0]} #{s[0][1]}) #{s[1][1]} #{s[0][2]}) #{s[1][2]} #{s[0][3]}"
-		else
-			"(#{s[0][0]} #{s[1][0]} #{s[0][1]}) #{s[1][1]} (#{s[0][2]} #{s[1][2]} #{s[0][3]})"
-		end
 	end
 end
